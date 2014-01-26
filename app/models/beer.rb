0684424c34a1d6f4966876645fc12f2c5cf1fba4
juhoesse <juho.esselstrom@helsinki.fi>
2014-01-26 23:50:46 +0200

    viikko 2

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,15 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  #ratings.to_a.inject(0.0) { |sum, el| sum + el} /ratings.size
+  def average_rating
+    sum = 0
+    ratings.each do |r|
+       sum += r.score
+    end
+
+    average = 0.0
+    average = sum / ratings.count
+  end
 end
