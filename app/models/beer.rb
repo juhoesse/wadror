2653bc68a0d62abb5e50275139d5053adf4f35a1
juhoesse <juho.esselstrom@helsinki.fi>
2014-02-02 17:51:04 +0200

    Viikko2

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,14 +1,7 @@
 class Beer < ActiveRecord::Base
+  include RatingAverage
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
-
-  def average_rating
-    sum = 0
-    ratings.each do |r|
-       sum += r.score
-    end
-    sum / ratings.count
-  end
 
   def to_s
     self.name + ', ' + brewery.name
